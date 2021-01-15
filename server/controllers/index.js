7d1f8c6bd458db791d2fa03408eae5a692d6e29c
vbaobao <vbao.main@gmail.com>
2021-01-14 17:22:02 -0800

    Wrote code for controller and models

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,24 +2,28 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) { 
+      console.log(`controllers/messages/GET status code: ${res.statusCode}`);
+      models.messages.get();
+    }, // a function which handles a get request for all messages
     post: function (req, res) {
+      console.log(`controllers/messages/POST status code: ${res.statusCode}`);
       console.log('MESSAGES CONTROLLER Req:', req.body.message);
       // create vars for req json values
       // pass vals to proper models/index function
-      // models.messages.post(message);
+      models.messages.post(req.body);
     } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {
-      //console.log('USERS CONTROLLER Req:', req.body.username);
-      // create vars for req json values
-      // pass vals to proper models/index function
+    get: function (req, res) {
+      console.log(`controllers/users/GET status code: ${res.statusCode}`);
+      models.users.get();
+    },
+    post: function (req, res) { 
+      console.log(`controllers/users/POST status code: ${res.statusCode}`);
       models.users.post(req.body.username);
     }
   }
 };
-
--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,57 @@
+const connection = require('../db');
 var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      let sql = `SELECT message FROM messages`;
+
+      connection.query(sql, (err, results, fields) => {
+        if (err) { return console.error(err.message); }
+        console.log(`RESULT OF GET MESSAGES: ${results}`);
+      });
+    }, // a function which produces all the messages
+    post: function (message) {
+      // Query to insert the new room
+      let sqlRoom = `INSERT rooms (roomname) VALUES ('${message.roomname}')`;
+      connection.query(sqlRoom, (err, results, fields) => {
+        if (err) { return console.error(err.message); }
+        console.log(`Rows affected: ${results.affectedRows}`);
+      });
+
+      // Query to insert new message
+      let sqlMessage = `
+        SET @roomname = (SELECT id FROM rooms WHERE roomname = '${message.roomname}');
+        SET @username = (SELECT id FROM users WHERE username = '${message.username}');
+        INSERT messages (message, roomname, username) VALUES
+        (SELECT ${message.message}, @roomname, @username)
+      `;
+
+      connection.query(sqlMessage, (err, results, fields) => {
+        if (err) { return console.error(err.message); }
+        console.log(`Rows affected: ${results.affectedRows}`);
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      let sql = `SELECT username FROM users`;
+
+      connection.query(sql, (err, results, fields) => {
+        if (err) { return console.error(err.message); }
+        console.log(`RESULT OF GET USERS: ${results}`);
+      });
+    },
+    post: function (username) {
+      let sql = `INSERT users (username) VALUES ('${username}')`;
+
+      connection.query(sql, (err, results, fields) => {
+        if (err) { return console.error(err.message); }
+        console.log(`Rows affected in users table: ${results.affectedRows}`);
+      });
+    }
   }
 };
 
