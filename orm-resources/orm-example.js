03b9164cb731ad73682a0e43069844bb51c03585
vbaobao <vbao.main@gmail.com>
2021-01-15 19:18:32 -0800

    Tinker with Sequelize

--- orm-resources/orm-example.js
+++ orm-resources/orm-example.js
@@ -5,20 +5,25 @@
 
 var Sequelize = require('sequelize');
 var db = new Sequelize('chat', 'root', '');
+
 /* TODO this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
 /* first define the data structure by giving property names and datatypes
  * See http://sequelizejs.com for other datatypes you can use besides STRING. */
-var User = db.define('User', {
+var User = db.define('users', {
   username: Sequelize.STRING
 });
 
-var Message = db.define('Message', {
-  userid: Sequelize.INTEGER,
-  text: Sequelize.STRING,
-  roomname: Sequelize.STRING
+var Message = db.define('messages', {
+  username: Sequelize.INTEGER,
+  message: Sequelize.STRING
 });
+
+User.hasMany(Message, { foreignKey: 'username', sourceKey: 'id' });
+Message.belongsTo(User, { foreignKey: 'username', targetKey: 'id' });
+
+// 
 
 /* Sequelize comes with built in support for promises
  * making it easy to chain asynchronous operations together */
@@ -35,10 +40,27 @@ User.sync()
     users.forEach(function(user) {
       console.log(user.username + ' exists');
     });
+  })
+  .then(() => {
+    Message.sync();
+  })
+  .then( () => {
+    return User.findAll({where: {username: 'Jean Valjean'}});
+  })
+  .then(function(userId) {
+    console.log('USER ID: ', userId);
+    return Message.create({message: 'My sword is missing.', username: userId.id});
+  })
+  .then(function() {
+    return Message.findAll({where: {username: '1'}});
+  })
+  .then(function(messages) {
+    messages.forEach((message) => {
+      console.log(message.message + ', but who cares.');
+    });
     db.close();
   })
   .catch(function(err) {
-    // Handle any error in the chain
-    console.error(err);
+    console.log(err);
     db.close();
   });
